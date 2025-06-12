4f1fc7ff2c8164174daff14b5e1341d6daaee562
Aaron Sierp <aaronsierp@gmail.com>
2025-06-12 12:16:20 -0400

    updated background image to be pulled from unsplash api

--- public/js/index.js
+++ public/js/index.js
@@ -5,41 +5,62 @@ const elements = {
     author: document.getElementById("author")
 };
 
-const quotes = [
-    {
-        quote: "To Infinity and Beyond!",
-        author: "Buzz Lightyear"
-    },
+async function getRandomImage(){
+    const client_id = "2rFxFW6XjaO6Fohk52h9cxk0B4Aj7iB3cLxRAJ0UMcw"
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try{
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
 
-    {
-        quote: "I'd buy that for a dollar",
-        author: "Bandit Heeler"
-    },
-
-    {
-        quote: "A penny saved is a penny earned",
-        author: "Benjamin Franklin"
-    },
-
-    {
-        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
-        author: "Eric Schmidt"
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl})`;
     }
-]
-
-function loopThroughQuotes(){
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quote.length){
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        }
-        else{
-            quoteIndex = 0;
-        }
-
-    }, 3000);
+    catch(error){
+        console.error(error);
+    }
 }
 
-setTimeout(loopThroughQuotes, 3000);
+getRandomImage();
+
+
+
+
+// const quotes = [
+//     {
+//         quote: "To Infinity and Beyond!",
+//         author: "Buzz Lightyear"
+//     },
+
+//     {
+//         quote: "I'd buy that for a dollar",
+//         author: "Bandit Heeler"
+//     },
+
+//     {
+//         quote: "A penny saved is a penny earned",
+//         author: "Benjamin Franklin"
+//     },
+
+//     {
+//         quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+//         author: "Eric Schmidt"
+//     }
+// ]
+
+// function loopThroughQuotes(){
+//     let quoteIndex = 0;
+//     setInterval(() => {
+//         if (quoteIndex < quotes.length){
+//             elements.quote.textContent = quotes[quoteIndex].quote;
+//             elements.author.textContent = quotes[quoteIndex].author;
+//             quoteIndex++;
+//         }
+//         else{
+//             quoteIndex = 0;
+//         }
+
+//     }, 3000);
+// }
+
+// setTimeout(loopThroughQuotes, 3000);
